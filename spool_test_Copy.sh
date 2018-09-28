b5723605a5a80bdb4ea6409a6909b59daaf970af
Brian <renzhe3@huawei.com>
2018-09-28 18:03:02 +0200

    first change

---
+++ spool_test_Copy.sh
@@ -0,0 +1,31 @@
+sqlplus ¨CS user/passwd@10.xx.xx.xx:xxx/xxx >/dev/null <<EOF
+
+set serveroutput on
+
+set feedback off
+
+set heading off
+
+set trims on
+
+set pagesize 0
+
+set timing off
+
+set lin 5000
+
+spool data.unl
+
+select sysdate from dual;
+
+spool off;
+
+set serveroutput off
+
+EOF
+
+new 
+new1
+new2
+
+sdljf
